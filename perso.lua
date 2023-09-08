c1f09ae9c2d7ac80d030b0fe68e7e26d9c36c029
PINGUET Julien <pinguet62@gmail.com>
2023-09-08 16:48:44 +0200

    builders for generation & simple itération

--- perso.lua
+++ perso.lua
@@ -9,23 +9,61 @@ laPopulation = {}
 lesBoutons = { "A", "B", "X", "Y", "Up", "Down", "Left", "Right", }
 -- size: #(lesBoutons)
 
+function newReseau()
+    return {}
+end
+
+function newPopulation()
+    population = {}
+    for i = 0, NB_INDIVIDU_POPULATION do
+        table.insert(population, newReseau())
+    end
+    return population
+end
+
+function updateOutput()
+    -- random
+    --newButtons = {}
+    --for i, k in ipairs(lesBoutons) do
+    --    newButtons[k] = ({ true, false })[math.random(1, 2)]
+    --end
+
+    newButtons = { Right = true }
+
+    joypad.set(newButtons, 1)
+    emu.frameadvance()
+end
+
 function startLevel()
     savestate.load(NOM_SAVESTATE)
 end
 
-console.clear()
-console.log("Starting...")
-startLevel()
-
-function randomButtons()
-    newButtons = {}
-    for i, k in ipairs(lesBoutons) do
-        newButtons[k] = ({ true, false })[math.random(1, 2)]
-    end
-    return newButtons
+function niveauFini()
+    return memory.readbyte(0x0100) == 12
 end
 
-while true do
-    joypad.set(randomButtons(), 1)
-    emu.frameadvance()
+function getScore()
+    return math.random(0, 100)
+end
+
+function play()
+    while true do
+        updateOutput()
+        if niveauFini() then
+            return getScore()
+        end
+    end
+end
+
+console.clear()
+console.log("Starting...")
+for generation = 1, 5 do
+    console.log("Génération " .. generation)
+    population = newPopulation()
+    for i, individu in ipairs(population) do
+        console.log("\tIndividu " .. i)
+        startLevel()
+        score = play()
+        console.log("\t\tScore " .. score)
+    end
 end
